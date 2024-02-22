bb9b49c3fe4c17db1cbdf78b29fbb40226f2fcbc
yiber <yiber@LUZMORANTES>
2024-02-22 16:17:36 -0500

    commit 3

--- Proyecto de Prueba/Controllers/CiudadController.cs
+++ Proyecto de Prueba/Controllers/CiudadController.cs
@@ -1,6 +1,47 @@
-﻿namespace Proyecto_de_Prueba.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_de_Prueba.Models;
+using System.Data.SqlClient;
+
+namespace Proyecto_de_Prueba.Controllers
 {
-    public class CiudadController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CiudadController : ControllerBase
     {
+        public readonly string con;
+        public CiudadController(IConfiguration configuration)
+        {
+            con = configuration.GetConnectionString("conexion");
+        }
+        [HttpGet]
+        public IEnumerable<Ciudad> Get()
+        {
+            List<Ciudad> ciudad = new();
+            using (SqlConnection connection = new(con))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new("ListarCiudades", connection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Ciudad p = new Ciudad
+                            {
+                                NombreCiudad = reader["nombreCiudad"].ToString(),
+                              
+                                
+
+                            };
+                            ciudad.Add(p);
+                        }
+                    }
+                }
+                return ciudad;
+            }
+        }
     }
 }
